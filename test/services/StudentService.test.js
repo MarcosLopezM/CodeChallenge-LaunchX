eb1c100a89dc11db3044449dc54423ac6ae462f6
MarcosLopez <marcoslm@ciencias.unam.mx>
2022-05-03 07:06:20 -0500

    Adding unit tests for filterCertificatedStudents method

---
+++ test/services/StudentService.test.js
@@ -0,0 +1,24 @@
+const StudentService = require("./../../lib/services/StudentService");
+
+describe("Requirement 2: Get students' emails if they have a certification", () => {
+    test("Testing if the method filterCertificatedStudents returns an object", () => {
+        const students = require("./certificated_students.json");
+        const result = StudentService.filterCertificatedStudents(students);
+
+        expect(typeof result).toMatch(/object/);
+    });
+
+    test("Testing if the method filterCertificatedStudents returns an email of a student", () => {
+        const students = require("./certificated_students.json");
+        const result = StudentService.filterCertificatedStudents(students);
+
+        expect(result[0]).toMatch(/Todd@visualpartnership.xyz/);
+    });
+
+    test("Testing other properties of the method filterCertificatedStudents", () => {
+        const students = require("./certificated_students.json");
+        const result = StudentService.filterCertificatedStudents(students);
+
+        expect(result.length).toBe(6);
+    });
+});
