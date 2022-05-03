7820b7d379e8182c2455cb2ae6e5f8ed1b9489e7
MarcosLopez <marcoslm@ciencias.unam.mx>
2022-05-03 09:00:27 -0500

    Adding unit tests for filterStudentsByCredits method

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -22,3 +22,22 @@ describe("Requirement 2: Get students' emails if they have a certification", () 
         expect(result.length).toBe(5);
     });
 });
+
+describe("Requirement 3: Get students with over 500 credits", () => {
+    test("Testing if there is at least one student with over 500 credits", () => {
+        const students = require("./certificated_students.json");
+        const result = StudentService.filterStudentsByCredits(students);
+
+        expect(result.length).toBeGreaterThan(1);
+    });
+
+    test("Testing if attributes of elements of return object are correct", () => {
+        const students = require("./certificated_students.json");
+        const result = StudentService.filterStudentsByCredits(students);
+
+        expect(result[0].name).toMatch(/Warren/);
+        expect(result[0].email).toMatch(/Todd@visualpartnership.xyz/);
+        expect(result[0].credits).toBeGreaterThan(500);
+        expect(result[0].id).not.toBeNull();
+    });
+});
