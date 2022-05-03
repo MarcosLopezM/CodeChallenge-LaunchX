19210739deaeecef40384682e0df963db4875ec3
MarcosLopez <marcoslm@ciencias.unam.mx>
2022-05-03 00:32:47 -0500

    Changing database file from partners.json to students.json

--- test/utils/reader.test.js
+++ test/utils/reader.test.js
@@ -2,7 +2,7 @@ const Reader = require("./../../lib/utils/reader");
 
 describe("Reader test suite", () => {
     test("1) Read a file", () => {
-        const partners = Reader.readJsonFile("partners.json");
+        const partners = Reader.readJsonFile("students.json");
 
         expect(partners[0].name).toMatch(/Warren/);
         expect(partners.length).toBe(51);
